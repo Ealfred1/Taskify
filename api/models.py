b75bd20a09a363c28abe852763974bc3f3e49506
ealfred1 <alfrederic371@gmail.com>
2023-12-14 01:57:36 +0100

    Added the models

--- api/models.py
+++ api/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Task(models.Model):
+	title = models.CharField(max_length=100)
+	description = models.TextField()
+	due_date = models.DateField()
+	priority = models.CharField(max_length=20, default='Medium')
+	user = models.ForeignKey(User, on_delete=models.CASCADE)
+	date_created = models.DateTimeField(auto_now_add=True)
+	
+	def __str__(self):
+		return str(self.title)
