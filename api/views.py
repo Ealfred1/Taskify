af5b4c4f7db70f9e8837f47dac9cf5810ec32f6e
ealfred1 <alfrederic371@gmail.com>
2023-12-17 00:38:08 +0100

    just some boilerplate code

--- api/views.py
+++ api/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
 
-# Create your views here.
+
+@api_view(['GET'])
+def test_response(request):
+	response = {
+		'Packages': 'package1'
+	}
+	return Response(response)
