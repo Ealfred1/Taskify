4bbff7d0e7c5c89f3bd9b7688cfd0375ad9d93c6
ealfred1 <alfrederic371@gmail.com>
2023-12-17 02:57:48 +0100

    just some boilerplate code

--- taskify/settings.py
+++ taskify/settings.py
@@ -146,6 +146,10 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5173",
+]
+
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
